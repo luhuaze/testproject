19ea8768b71c50c42a98ff0dfef06001a68d464a
yuanyumei93 <1305277699@qq.com>
2021-04-26 00:03:55 +0800

    test-commit

---
+++ src/main/java/com/example/lhz/LhzApplication.java
@@ -0,0 +1,13 @@
+package com.example.lhz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lhz/LhzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lhz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
